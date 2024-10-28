a2a2db4413172c0a7f14496469bcfbc6c6118784
Antonio Jesús Padillo <98108563+AJPadillo@users.noreply.github.com>
2024-10-28 10:58:23 +0000

    actualizado 1 models.py

--- src/api/models.py
+++ src/api/models.py
@@ -6,8 +6,9 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
